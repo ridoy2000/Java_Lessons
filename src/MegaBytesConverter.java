b6eecf90e3fecf423380056cba1aab6892edcf79
ridoy2000 <ridoy.asad3000@gmail.com>
2025-03-30 00:29:16 +0000

    Committing completed method exercises

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -2,6 +2,20 @@ public class MegaBytesConverter {
 
     public static void main(String[] args) {
 
+
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }else {
+            int megabytes = kiloBytes / 1024;
+            int remaining_kilobytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remaining_kilobytes + " KB" );
+        }
+
     }
 
 
